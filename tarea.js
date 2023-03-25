e52b70d1feb0a7a088b96e2ad6f6ee76941bc708
CMBroll <mbroll25@gmail.com>
2023-03-25 20:11:45 -0300

    Corrigiendo tarea Javascript

--- tarea.js
+++ tarea.js
@@ -1,5 +1,5 @@
 /* 1) Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.  */
-alert("un mensaje")
+alert("Primera tarea de Javascript")
 
 /* 2) Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write). */
 document.write("Hello World")
