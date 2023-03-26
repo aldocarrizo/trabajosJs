2411ff92a29cd8e74fb4af6c46ad787807e81419
Esteban <esteban.cat129@gmail.com>
2023-03-26 15:34:05 -0300

    Se realizo la tarea de la clase del dia 23/03

---
+++ estebanPedrazaJs/miPrimeraJS.js
@@ -0,0 +1,31 @@
+    {
+    alert ("Hola mundo")
+    document.write ("Hello World" + " ")
+}
+
+function sumar() {
+    const num1 = 3
+    const num2 = 5
+    const resultado = num1 + num2
+    document.write ("El resultado escrito es" + " " +resultado)
+}
+
+sumar()
+
+let username =  prompt ("Ingrese su nombre de usuario")
+
+function ingresarDatos(username){
+    alert("Hola " + username)
+}
+
+ingresarDatos(username)
+
+let num1 = parseInt(prompt("Ingresa el numero"))
+let num2 = parseInt(prompt("Ingresa el segundo numero"))
+
+function suma(num1, num2){
+    let resultado = num1 + num2
+    alert("El resultado es" + " " + resultado)
+}
+
+suma(num1, num2)
