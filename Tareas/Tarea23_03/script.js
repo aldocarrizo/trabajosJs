3b74898dde5ceb775ab613c43cd8a1aa50f50dc8
SantiagoECastillo <castillosantiagoe1998@gmail.com>
2023-03-26 13:17:50 -0300

    resueltos Punto 3 y 4

--- Tareas/Tarea23_03/script.js
+++ Tareas/Tarea23_03/script.js
@@ -2,7 +2,7 @@
 
 // 1) Escribe un programa de una sola línea que haga que aparezca en la pantalla
 //    un alert que diga “un mensaje”. 
-window.alert("Un mensaje");
+//window.alert("Un mensaje");
 
 
 // 2) Escribe un programa de una sola línea que
@@ -11,12 +11,13 @@ document.write("Hello World");
 
 // 3) Escribe un programa de una sola línea que escriba en la pantalla el
 //    resultado de sumar 3 + 5. 
-
+document.write("<br> El resultado de sumar 3 + 5 es: ", 3+5);
 
 // 4) Escribe un programa de dos líneas que pida el
 //    nombre del usuario con un prompt y escriba un texto que diga «Hola
 //    nombreUsuario» 
-
+let nombreUsuario = prompt("Ingrese su nombre: ");
+document.write("<br> Hola ", nombreUsuario);
 
 // 5) Escribe un programa de tres líneas que pida un número, pida
 //    otro número y escriba el resultado de sumar estos dos números
